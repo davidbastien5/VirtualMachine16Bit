87f99764c74affdabea9da5c1193179c844a7f0b
David Bastien <david.bastien5@gmail.com>
2020-10-12 17:33:50 -0400

    Virtual machine test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::process;
-use virtual_machine16_bit::{cpu::CPU, memory};
+use virtual_machine16_bit::{cpu::CPU, instructions, memory};
 
 fn main() {
     process::exit(match run() {
@@ -12,8 +12,27 @@ fn main() {
 }
 
 fn run() -> Result<(), String> {
-    let cpu = CPU::new(memory::create_memory(1))?;
-    println!("{:?}", cpu);
-    println!("{}", cpu.get_register("ip")?);
+    let mut memory = memory::create_memory(256);
+
+    memory[0] = instructions::MOV_LIT_R1;
+    memory[1] = 0x12; // 0x1234
+    memory[2] = 0x34;
+
+    memory[3] = instructions::MOV_LIT_R2;
+    memory[4] = 0xAB; // 0xABCD
+    memory[5] = 0xCD;
+
+    memory[6] = instructions::ADD_REG_REG;
+    memory[7] = 2; // r1 index
+    memory[8] = 3; // r2 index
+
+    let mut cpu = CPU::new(memory);
+    println!("{:#?}", cpu);
+    cpu.step()?;
+    println!("{:#?}", cpu);
+    cpu.step()?;
+    println!("{:#?}", cpu);
+    cpu.step()?;
+    println!("{:#?}", cpu);
     Ok(())
 }
