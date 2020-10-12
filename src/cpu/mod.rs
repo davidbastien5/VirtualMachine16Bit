b95f8ddfd739070ba3d3b4d82b61a51136232faa
David Bastien <david.bastien5@gmail.com>
2020-10-12 16:10:51 -0400

    Add docs to the CPU struct

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -3,7 +3,6 @@ use std::collections::HashMap;
 
 const REGISTER_NAMES: [&str; 10] = ["ip", "acc", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8"];
 
-#[derive(Debug)]
 pub struct CPU {
     memory: Box<[u8]>,
     register_map: HashMap<String, usize>,
@@ -11,6 +10,8 @@ pub struct CPU {
 }
 
 impl CPU {
+
+    /// Creates a new CPU instance with the given memory.
     pub fn new(memory: Box<[u8]>) -> Result<CPU, String> {
         let register_map: HashMap<String, usize> = REGISTER_NAMES
             .iter()
@@ -27,6 +28,7 @@ impl CPU {
         })
     }
 
+    /// Gets the value in the given register.
     pub fn get_register(&self, name: &str) -> Result<u16, String> {
         let index = self
             .register_map
@@ -37,6 +39,7 @@ impl CPU {
         Ok(u16::from_be_bytes(bytes))
     }
 
+    /// Sets the given value to the given register.
     pub fn set_register(&mut self, name: &str, value: u16) -> Result<(), String> {
         let index = self
             .register_map
@@ -49,6 +52,7 @@ impl CPU {
         Ok(())
     }
 
+    /// Fetches the next instruction.
     pub fn fetch(&mut self) -> Result<u8, String> {
         let instruction_address = self.get_register("ip")?;
         let instruction = self.memory[instruction_address as usize];
