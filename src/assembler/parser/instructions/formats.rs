01e25dcc2b48beb0a7178573addbc5dcad5da8d5
David Bastien <david.bastien5@gmail.com>
2020-10-27 20:38:35 -0400

    Add the missing common instruction parsers

--- src/assembler/parser/instructions/formats.rs
+++ src/assembler/parser/instructions/formats.rs
@@ -11,6 +11,22 @@ fn space_delimited_comma(input: &str) -> IResult<&str, char> {
     delimited(space0, char(','), space0)(input)
 }
 
+pub fn lit<F>(mnemonic: String, mapper: F) -> impl Fn(&str) -> IResult<&str, ast::Instruction>
+where
+    F: Fn(ast::Expr) -> ast::Instruction,
+{
+    move |input: &str| {
+        map(
+            delimited(
+                tuple((tag_no_case(&mnemonic[..]), space1)),
+                expressions::literal_expr,
+                space0,
+            ),
+            |literal_expr| mapper(literal_expr),
+        )(input)
+    }
+}
+
 pub fn lit_mem<F>(mnemonic: String, mapper: F) -> impl Fn(&str) -> IResult<&str, ast::Instruction>
 where
     F: Fn(ast::Expr, ast::Expr) -> ast::Instruction,
@@ -94,6 +110,29 @@ where
                 space0,
             ),
             |(address_expr, register)| mapper(address_expr, register),
+        )(input)
+    }
+}
+
+pub fn no_arg<F>(mnemonic: String, mapper: F) -> impl Fn(&str) -> IResult<&str, ast::Instruction>
+where
+    F: Fn() -> ast::Instruction,
+{
+    move |input: &str| map(tuple((tag_no_case(&mnemonic[..]), space0)), |_| mapper())(input)
+}
+
+pub fn reg<F>(mnemonic: String, mapper: F) -> impl Fn(&str) -> IResult<&str, ast::Instruction>
+where
+    F: Fn(ast::Register) -> ast::Instruction,
+{
+    move |input: &str| {
+        map(
+            delimited(
+                tuple((tag_no_case(&mnemonic[..]), space1)),
+                types::register,
+                space0,
+            ),
+            |register| mapper(register),
         )(input)
     }
 }
